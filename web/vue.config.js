344fdb95b43b58958f684d27a63d5d49029484b5
TonyBlur <zehao100@qq.com>
2024-06-08 04:47:15 +0800

    test

--- web/vue.config.js
+++ web/vue.config.js
@@ -5,7 +5,7 @@ const isLibrary = process.env.NODE_ENV === 'library'
 const WebpackDynamicPublicPathPlugin = require('webpack-dynamic-public-path')
 
 module.exports = {
-  publicPath: '/dist/',
+  publicPath: '/dist',
   outputDir: 'dist',
   lintOnSave: false,
   productionSourceMap: false,
